212cb09690869189af3894711c6891bb42bb89d0
David <david.rohusch@post.cz>
2022-04-02 07:46:26 +0200

    Příprava projektu

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def EncryptUid(uid):
+    pass
+def DecryptUid(uid):
+    pass
+
+def addUid(URL, uid):
+    pass
+
+
+def DecryptUidFromURL(URL, argName):
+    pass
+def EncryptUidToURL(URL, argName, uid):
+    pass
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+   pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
